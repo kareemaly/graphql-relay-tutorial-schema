3de505e0bbd92e67f5f37bc4a1c62da3757955a3
Kareem Mohamed <kareem3d.a@gmail.com>
2017-08-13 14:24:40 +0200

    Adding relay to the graphql schema and characters connection

--- server/graphqlSchema.js
+++ server/graphqlSchema.js
@@ -5,23 +5,53 @@ const {
 } = require('graphql');
 
 const {
+	globalIdField,
+	nodeDefinitions,
+	fromGlobalId,
+	connectionDefinitions,
+	connectionArgs,
+	connectionFromArray,
+} = require('graphql-relay');
+
+const {
 	getJonSnow,
 	getSansaStark,
+	getCharacterById,
+	getAllCharacters,
 } = require('./character');
+
+const { nodeField, nodeInterface } = nodeDefinitions((globalId) => {
+	const { type, id } = fromGlobalId(globalId);
+	switch(type) {
+		case 'Character':
+			return getCharacterById(id);
+	}
+})
 
 const characterType = new GraphQLObjectType({
 	name: 'Character',
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: globalIdField('Character'),
 		name: { type: GraphQLString },
 		title: { type: GraphQLString },
 	}),
+	interfaces: [ nodeInterface ],
+	isTypeOf: (character) => true
 });
+
+const { connectionType } = connectionDefinitions({ nodeType: characterType });
 
 module.exports = new GraphQLSchema({
 	query: new GraphQLObjectType({
 		name: 'Query',
 		fields: () => ({
+			characters: {
+				type: connectionType,
+				args: connectionArgs,
+				resolve: (_, pArgs) =>
+					connectionFromArray(getAllCharacters(), pArgs)
+			},
+			node: nodeField,
 			jonSnow: {
 				type: characterType,
 				resolve: getJonSnow,
